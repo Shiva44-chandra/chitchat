8812b6d3fc42a63b86f19e1a68adaf7cc63c700e
shiva chandra <shivachandra874@gmail.com>
2021-12-28 11:08:30 +0530

    view engine is set up

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+//use express router
+app.use('/',require('./routes/index')); 
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log('Error in setting server:',err);
+    } 
+
+    console.log('Server is set on port number:',port);
+});
