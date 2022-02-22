eb0f859e8dabac1b7ceda52ed7b366a839c0373c
shiva chandra <you@example.com>
2022-02-22 20:20:15 +0530

    production log

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,14 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
 
+const logDirectory = path.join(__dirname,'../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accesslogStream  =  rfs.createStream('access.log',{
+    interval:'1d',
+    path:logDirectory
+});
 
 const development  ={
     name:'development',
@@ -8,15 +18,33 @@ const development  ={
     google_client_ID:      "346696613344-t90bip873iarpttl0b57u25dnip085ck.apps.googleusercontent.com",
     google_client_Secret:   "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",
     google_callback_URL:    "http://localhost:8000/users/auth/google/callback",
-    jwt_secret:'chitchat'
+    jwt_secret:'chitchat',
+    morgan:{
+        mode:'dev',
+        options:{stream: accesslogStream}
+    }
 }
 
 const production ={
-    name:'production'
+    name:'production',
+    asset_path:process.env.ASSET_PATH,
+    session_cookie_key:process.env.Chitchat_Session_cookie,
+    db:'chitchat_production',
+    google_client_ID:      process.env.Chitchat_Google_Client_Id,
+    google_client_Secret:   process.env.Chitchat_Google_Client_Secret,
+    google_callback_URL:    process.env.Chitchat_Google_Client_URL,
+    jwt_secret:process.env.JWT_SECRET,
+    morgan:{
+        mode:'combined',
+        options:{stream: accesslogStream}
+    }
+
+    
 }
 
 
+//console.log(process.env);
 
 
-
-module.exports = development; 
+module.exports = eval(process.env.Chitchat_Environment) == undefined ? development : eval(process.env.Chitchat_Environment); 
+//module.exports = development;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const env = require('./config/environment');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000; 
@@ -23,7 +24,9 @@ app.use(cookieParser());
 app.use(express.static(env.asset_path));
 
 //make the uploads path available to the browser 
-app.use('/uploads',express.static( __dirname + '/uploads'));
+app.use('/uploads',express.static( __dirname + '/uploads')); 
+
+app.use(logger(env.morgan.mode,env.morgan.options));
 
 app.use(expressLayouts);
 
